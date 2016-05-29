3367c7b0da6c89415905c5fc209de5e242f49ec8
Ben Dalling <b.dalling@locp.co.uk>
2016-05-29 20:01:50 +0100

    Adding test coverage.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,2 +1,20 @@
+# == Class opscenter::params
+#
+# This class is meant to be called from opscenter
+# It sets variables according to platform
+#
 class opscenter::params {
+  case $::osfamily {
+    'Debian': {
+      $systemctl = '/bin/systemctl'
+      $systemd_path = '/lib/systemd/system'
+    }
+    'RedHat': {
+      $systemctl = '/usr/bin/systemctl'
+      $systemd_path = '/usr/lib/systemd/system'
+    }
+    default: {
+      $config_path_parents = []
+    }
+  }
 }
---
+++ spec/classes/params_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'opscenter::params' do
+  context 'with default values for all parameters' do
+    let :facts do
+      {
+        osfamily: 'RedHat'
+      }
+    end
+
+    it do
+      should compile
+      should contain_class('opscenter::params')
+      should have_resource_count(0)
+    end
+  end
+end
